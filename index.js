9c2998f41c9e6cce9f1745d89d1dc346018674c1
Reydel Ocon <reydel321@gmail.com>
2021-11-06 00:28:40 +0800

    yow

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const navContainer = document.getElementById('yoww')
 mobNav.addEventListener('click', () => {
     navContainer.classList.toggle('show');
 });
+
+
+const darkMode = document.querySelector('#switch__dark')
+
+darkMode.addEventListener('click', (y) => {
+    document.body.classList.toggle('darkmode')
+})
