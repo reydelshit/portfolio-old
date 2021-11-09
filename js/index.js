d757488cc1e9a466f47248af2c1977e0cf47a9a1
Reydel Ocon <reydel321@gmail.com>
2021-11-09 15:42:43 +0800

    test if okay mobile

--- js/index.js
+++ js/index.js
@@ -44,20 +44,20 @@ pageTransition.reveal('.description', {origin: 'left', delay: 2600})
 pageTransition.reveal('.icons__yow', {origin: 'right', delay: 2700})
 pageTransition.reveal('.image__container', {origin: 'right', delay: 3200})
 
-pageTransition.reveal('.projects__header', {origin: 'top', delay: 100})
-pageTransition.reveal('.project__item1', {origin: 'left',  delay: 300, distance: '500px'})
-pageTransition.reveal('.project__item2', {origin: 'right', delay: 300, distance: '500px'})
-pageTransition.reveal('.projects__footer', {origin: 'bottom', distance: '20px'})
+pageTransition.reveal('.projects__header', {origin: 'top', delay: 100 , mobile: false})
+pageTransition.reveal('.project__item1', {origin: 'left',  delay: 300, distance: '500px', mobile: false})
+pageTransition.reveal('.project__item2', {origin: 'right', delay: 300, distance: '500px', mobile: false})
+pageTransition.reveal('.projects__footer', {origin: 'bottom', distance: '20px', mobile: false})
 
-pageTransition.reveal('.techs__header h2', {origin: 'right', distance: '200px'})
-pageTransition.reveal('.techs__header p', {origin: 'left', distance: '200px'})
+pageTransition.reveal('.techs__header h2', {origin: 'right', distance: '200px', mobile: false})
+pageTransition.reveal('.techs__header p', {origin: 'left', distance: '200px', mobile: false})
 
-pageTransition.reveal('.one', {origin: 'left', delay: 1200, distance: '500px'})
+pageTransition.reveal('.one', {origin: 'left', delay: 1200, distance: '500px', mobile: false})
 pageTransition.reveal('.two', {origin: 'top', delay: 1500})
-pageTransition.reveal('.three', {origin: 'right', delay: 1900, distance: '500px'})
-pageTransition.reveal('.four', {origin: 'left', delay: 2500, distance: '500px'})
-pageTransition.reveal('.five', {origin: 'bottom', delay: 2000})
-pageTransition.reveal('.six', {origin: 'right', delay: 2500, distance: '500px'})
+pageTransition.reveal('.three', {origin: 'right', delay: 1900, distance: '500px', mobile: false})
+pageTransition.reveal('.four', {origin: 'left', delay: 2500, distance: '500px', mobile: false})
+pageTransition.reveal('.five', {origin: 'bottom', delay: 2000, mobile: false})
+pageTransition.reveal('.six', {origin: 'right', delay: 2500, distance: '500px', mobile: false})
 
 
 
